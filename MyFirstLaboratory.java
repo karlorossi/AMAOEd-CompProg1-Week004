0c516b74b79e91ee3182e02dca6a159a2b19adf1
karlorossi <147283413+karlorossi@users.noreply.github.com>
2024-02-16 13:05:07 -0500

    Add files via upload

---
+++ MyFirstLaboratory.java
@@ -0,0 +1,50 @@
+public class FirstLaboratory {
+
+    public static void main2(String[] args) {
+				System.out.println("This is my first laboratory exercise");
+    }
+
+}
+
+
+
+
+/*
+
+
+Test Stem / Question	Choices
+1: Open the generated file from the directory. What does it contain?	
+	A: Byte codes
+	
+2: What is the file name of the generated file?	
+	A: FirstLaboratory.class
+	
+3: What does the console say?	
+	A: This is my first laboratory exercise.
+	
+
+4: What happens?	
+	A: Code did not compile.
+	
+5: What is the file name of the generated file?	
+	D: There’s no generated file.
+
+6:  What happens?	
+	A: Code did not compile.
+	
+7: What is the file name of the generated file?	
+	D: There’s no generated file.
+
+
+8:  What happens?	
+	A: Code did not compile.
+	
+9: What is the file name of the generated file?	
+	D: MyFirstLaboratory.class
+
+10: What does the console say?	
+	C: Nothing
+	
+
+
+*/
